19e1aa86135f2ab78366f8d3492144247171e6ef
MichelangeloSaveriano <74253635+MichelangeloSaveriano@users.noreply.github.com>
2020-11-25 18:21:43 +0100

    Fixed language issues
    Uploaded the updated version of the dataset and added the language detecor in the html parser

--- html_parser.py
+++ html_parser.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import re
 import os
+from langdetect import detect
 
 
 def from_list_to_str(lis):
@@ -76,6 +77,16 @@ def from_html_to_tsv(html_path, tsv_path):
             tsv_file.close()
             return
         html_info = parse(html_path)
+        try:
+            if html_info['plot'] and (detect(html_info['plot']) != 'en'):
+                print('Not English:', html_info)
+                tsv_file.close()
+                return
+        except:
+            print('Exception', html_info)
+            tsv_file.close()
+            return
+
         # print the keys on file
         tsv_file.write('\t'.join(html_info.keys()))
         tsv_file.write('\n')
@@ -92,11 +103,11 @@ def from_html_to_tsv(html_path, tsv_path):
 
 def process_pages(starting_page, ending_page, page_folder='./data/', destination_folder='./data/tsv/',
                   html_prefix='book', tsv_prefix='article', n_book_per_page=100, starting_number=1,
-                  print_page=True, print_book=True):
+                  print_page=False, print_book=False):
     for num_page in range(starting_page, ending_page + 1):
         if print_page:
             print('Page:', num_page)
-        for i in range(n_book_per_page):
+        for i in range(19704, n_book_per_page):
             if print_book:
                 print('Book:', i)
             idx = str((num_page - 1) * n_book_per_page + i + starting_number)
